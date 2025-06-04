acd3396b034293d7a78a1e30a3ff947d2599ee64
YoomJJu <fdgh134@naver.com>
2025-06-04 17:24:26 +0900

    fix: 루트 주소 멘트 추가

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.json());
 
 app.use('/api/items', itemsRoute);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 서버 실행 중: http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('✅ 마비노기 모바일일 아이템 API입니다. /api/items 또는 /api/ingredients를 사용하세요.');
 });
